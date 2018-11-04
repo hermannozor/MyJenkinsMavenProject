e3844f3a043c862879fe5d5d8ed8f963b89e4955
Hermannozor <hermannfotso2@gmail.com>
2018-11-04 13:32:56 +0100

    Initial commit

---
+++ src/main/java/com/itic/MyJenkinsMavenProject/App.java
@@ -0,0 +1,13 @@
+package com.itic.MyJenkinsMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
